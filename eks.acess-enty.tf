bcc18620e00a00b5cc287082aa254e2128bcfdf1
Paulo Ferreira <mrpauloii@gmail.com>
2025-02-22 12:43:16 -0300

    :construction_worker: Ateração no ARN

--- eks.acess-enty.tf
+++ eks.acess-enty.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_access_entry" "eks-access-entry" {
   cluster_name      = aws_eks_cluster.eks-cluster.name
-  principal_arn     = var.principalArn
+  principal_arn     = data.aws_iam_role.labrole.arn
   kubernetes_groups = ["fiap"]
   type              = "STANDARD"
 
--- vars.tf
+++ vars.tf
@@ -12,11 +12,6 @@ variable "instanceType" {
   default = "t3.micro"
 }
 
-variable "principalArn" {
-  description = "ARN do principal para acessar o cluster EKS"
-  type        = string
-  default = "arn:aws:iam::239569854352:role/voclabs"
-}
 
 variable "policyArn" {
   default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
